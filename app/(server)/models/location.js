1a95f80f4389ab65f7824de3222b1a9381acc5c3
sanjaysingh24 <sanjubora84@gmail.com>
2025-02-24 18:59:23 +0530

    update

---
+++ app/(server)/models/location.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const locationSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        trim:true
+    },
+    address:{
+        type:String,
+        trim:true
+    },
+    shop_id:{
+        type:Number,
+        default:1
+    },
+    isdefault:{
+        type:Boolean,
+        default:false
+    }
+
+
+})
